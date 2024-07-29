566fc140a2d4987c3148e239372b12e7c8135e4a
himanshu choudhary <148034602+himanshu1052@users.noreply.github.com>
2024-07-29 11:24:28 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -2,19 +2,14 @@ import express, { response } from "express";
 import mongoose from "mongoose";
 import dotenv from 'dotenv';
 dotenv.config();
-
 const server = express();
-
 let PORT = 3000;
-
-mongoose.connect(process.env.MONGODB_URI, { autoIndex: true })
-  .then(() => console.log('Connected to MongoDB'))
-  .catch(err => console.error('Could not connect to MongoDB', err));
-
+mongoose.connect(process.env.DB_LOCATION, {
+  autoIndex: true,
+});
 server.post("/signup", (req, res) => {
     res.json(req.body);
 })
-
 server.listen(PORT, () => {
   console.log("Listening on port: " + PORT);
 });
