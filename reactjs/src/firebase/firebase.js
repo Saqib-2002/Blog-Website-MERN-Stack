d38886b9097da02dc612ac9f27ac6edd7eb69b23
Saqib <saqibsaifi720@gmail.com>
2025-03-11 15:13:56 +0530

    removed cred.json to move keys to .env

--- reactjs/src/firebase/firebase.js
+++ reactjs/src/firebase/firebase.js
@@ -1,13 +1,13 @@
 import { initializeApp } from "firebase/app";
 import { getAuth, GoogleAuthProvider } from "firebase/auth";
-import json from "./cred.json";
+
 const firebaseConfig = {
-  apiKey: json.apiKey,
-  authDomain: json.authDomain,
-  projectId: json.projectId,
-  storageBucket: json.storageBucket,
-  messagingSenderId: json.messagingSenderId,
-  appId: json.appId,
+  apiKey: import.meta.env.VITE_API_KEY,
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_APP_ID,
 };
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
